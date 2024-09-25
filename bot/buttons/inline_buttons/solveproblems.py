34b63c4786b90b23bade759d59f6b813995c15bb
Samidillo Ravshanov <samidilloravshanov14@gmail.com>
2024-09-25 15:07:09 +0500

    requirements.txt

--- bot/buttons/inline_buttons/solveproblems.py
+++ bot/buttons/inline_buttons/solveproblems.py
@@ -12,7 +12,6 @@ def done_button1(telegram_id=None):
 
 
 def in_process_button(telegram_id=None):
-    print(telegram_id)
     button1 = InlineKeyboardButton(text='Jarayonda...', callback_data=f"in_process_{telegram_id}")
 
     builder = InlineKeyboardBuilder()
